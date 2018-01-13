9c8d6e9902f9b43760f8782fc9a289cb2fa32825
Yugma Moradia <yugma93@gmail.com>
2018-01-12 18:09:00 -0800

    adding gitignore to ignore the files to be untracked permanentally

---
+++ Java_Assignment/src/RootsQuadratic.java
@@ -0,0 +1,27 @@
+/**
+ * Created by ravi on 1/11/18.
+ */
+
+/**
+ * Find roots for the following quadratic equation.
+ The quadratic equation is a*X^2 - b*X + c
+ give different values to a,b and c. Find roots by using formula. (-b +/- sqrt(b^2 - 4*a*c) / 4* a*c)
+ If roots are imaginary then display no roots.
+ */
+import java.util.*;
+public class RootsQuadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, result, roots;
+
+        System.out.println("Enter the values for a,b and c");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+
+
+
+    }
+}
