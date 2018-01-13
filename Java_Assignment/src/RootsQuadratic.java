1fdd3a0e3417d61c2d4c47a6dab1ca167df59101
Yugma Moradia <yugma93@gmail.com>
2018-01-12 18:47:40 -0800

    adding roots quadratic

--- Java_Assignment/src/RootsQuadratic.java
+++ Java_Assignment/src/RootsQuadratic.java
@@ -9,19 +9,40 @@
  If roots are imaginary then display no roots.
  */
 import java.util.*;
+
+import static java.lang.Math.sqrt;
+
 public class RootsQuadratic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a, b, c, result, roots;
+        int a, b, c;
+        double root0,root1, root2, partialResult;
+
 
         System.out.println("Enter the values for a,b and c");
         a = sc.nextInt();
         b = sc.nextInt();
         c = sc.nextInt();
+        System.out.println("Given quadratic equation: "+a+"x^2 + "+b+"x + "+c);
 
+        partialResult = (b*b) - (4 * a * c);
 
+        if(partialResult>0) {
+            root1 = (-b + Math.sqrt(partialResult)) / (2 * a);
+            root2 = (-b - Math.sqrt(partialResult)) / (2 * a);
 
+            System.out.println(root1);
+            System.out.println(root2);
+        }
+        else if(partialResult==0){
+            root0 = (-b + Math.sqrt(partialResult)) / (2 * a);
+            System.out.println("Both roots are equal");
+            System.out.println(root0);
+        }
+        else{
+            System.out.println("Roots are imaginary");
+        }
 
     }
 }
